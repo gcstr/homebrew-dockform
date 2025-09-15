98f866905c2fd83e9245c0e28ca19cde92c8d4a1
Gustavo Castro <github@gustavocastro.com>
2025-09-16 01:40:45 +0200

    Update formula with correct SHA256 checksum

--- Formula/dockform.rb
+++ Formula/dockform.rb
@@ -2,7 +2,7 @@ class Dockform < Formula
   desc "Manage Docker Compose projects declaratively"
   homepage "https://dockform.io"
   url "https://github.com/gcstr/dockform/releases/download/v0.1.0/dockform_v0.1.0_darwin_amd64.tar.gz"
-  sha256 "placeholder_sha256_will_be_updated_by_goreleaser"
+  sha256 "24ebec0fc2ed9dbff88be6e9c87c630ac9d02e304ede85f202d4cbbb6ca902e7"
   license "MIT"
   head "https://github.com/gcstr/dockform.git", branch: "main"
 
