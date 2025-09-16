dcf0427c8f39a803efba9d7bbf605f6faeb8d102
Gustavo Castro <github@gustavocastro.com>
2025-09-16 04:09:30 +0200

    dockform: bump to v0.2.1 (arm64+amd64)

--- Formula/dockform.rb
+++ Formula/dockform.rb
@@ -1,12 +1,17 @@
 class Dockform < Formula
   desc "Manage Docker Compose projects declaratively"
   homepage "https://dockform.io"
-  url "https://github.com/gcstr/dockform/releases/download/v0.1.0/dockform_v0.1.0_darwin_amd64.tar.gz"
-  sha256 "24ebec0fc2ed9dbff88be6e9c87c630ac9d02e304ede85f202d4cbbb6ca902e7"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/gcstr/dockform/releases/download/v0.2.1/dockform_v0.2.1_darwin_arm64.tar.gz"
+      sha256 "763a562603b2cd1f7cefb03ad5630b489dce1c071c813bbe9ed86e7832490600"
+    else
+      url "https://github.com/gcstr/dockform/releases/download/v0.2.1/dockform_v0.2.1_darwin_amd64.tar.gz"
+      sha256 "229f0f30828b5edb02002a223bb8cf9cb22d7e748730242a5d0990b218ec4126"
+    end
+  end
   license "MIT"
   head "https://github.com/gcstr/dockform.git", branch: "main"
-
-  depends_on "go" => :build
 
   def install
     bin.install "dockform"
